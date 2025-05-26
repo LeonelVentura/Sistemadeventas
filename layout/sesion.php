72515601b7234a45352cc9747da46b563115f5a0
Leonel Ventura Lopez <leoventura1211@gmail.com>
2025-05-25 19:59:00 -0500

    first commit

---
+++ layout/sesion.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+if(isset($_SESSION['sesion email'])){
+  //echo "si existe sesion de ".$_SESSION['sesion email'];
+  $email_sesion = $_SESSION['sesion email'];
+  $sql = "SELECT * FROM tb_usuarios WHERE email = '$email_sesion'";
+  $query = $pdo->prepare($sql);
+  $query->execute();
+  $usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($usuarios as $usuario){
+    $nombres_sesion = $usuario['nombres'];
+  }
+  
+}else{
+  echo "no existe sesion";
+  header('Location: '.$URL.'/login');
+}
