9c995ee9e2fe03ed7ee3867b8f223a9082ecfa79
FernandoLoza01 <fernandolozabenites@gmail.com>
2025-06-02 12:48:25 -0500

    Actualizo base de datos

---
+++ sistemadeventas.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-06-2025 a las 19:44:08
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sistemadeventas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_almacen`
+--
+
+CREATE TABLE `tb_almacen` (
+  `id_producto` int(11) NOT NULL,
+  `codigo` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripción` text DEFAULT NULL,
+  `stock` int(11) NOT NULL,
+  `stock_maximo` int(11) DEFAULT NULL,
+  `precio_compra` varchar(255) NOT NULL,
+  `precio_venta` varchar(255) NOT NULL,
+  `fecha_ingreso` date NOT NULL,
+  `imagen` text DEFAULT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `stock_minimo` int(11) DEFAULT NULL,
+  `fyh_creacion` datetime NOT NULL,
+  `fyh_actualizacion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_categorias`
+--
+
+CREATE TABLE `tb_categorias` (
+  `id_categoria` int(11) NOT NULL,
+  `nombre_categoria` varchar(255) NOT NULL,
+  `fyh_creacion` datetime NOT NULL,
+  `fyh_actualizacion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tb_categorias`
+--
+
+INSERT INTO `tb_categorias` (`id_categoria`, `nombre_categoria`, `fyh_creacion`, `fyh_actualizacion`) VALUES
+(1, 'JUGO PAPAYA', '2025-06-01 23:23:19', '2025-06-01 22:05:23'),
+(2, 'JUGO PIÑA', '2025-06-01 18:54:14', '2025-06-01 22:05:28'),
+(4, 'PASTELITOS', '2025-06-01 18:55:47', '0000-00-00 00:00:00'),
+(11, 'lala', '2025-06-01 19:02:16', '0000-00-00 00:00:00'),
+(12, 'fff', '2025-06-01 19:04:33', '0000-00-00 00:00:00'),
+(13, 'HELADOS', '2025-06-01 19:09:16', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_roles`
+--
+
+CREATE TABLE `tb_roles` (
+  `id_rol` int(11) NOT NULL,
+  `rol` varchar(255) NOT NULL,
+  `fyh_creacion` datetime NOT NULL,
+  `fyh_actualizacion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tb_roles`
+--
+
+INSERT INTO `tb_roles` (`id_rol`, `rol`, `fyh_creacion`, `fyh_actualizacion`) VALUES
+(1, 'ADMINISTRADOR', '2025-05-26 01:04:37', '2025-05-26 01:04:37'),
+(3, 'CONTADOR', '2025-05-25 19:00:54', '2025-05-25 22:08:28'),
+(4, 'COCINERO', '2025-05-25 19:06:22', '2025-05-25 21:47:24'),
+(5, 'OPERADOR', '2025-05-25 22:41:22', '0000-00-00 00:00:00'),
+(6, 'ALMACEN', '2025-05-25 23:37:06', '0000-00-00 00:00:00'),
+(7, 'lala', '2025-06-01 15:45:45', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_usuarios`
+--
+
+CREATE TABLE `tb_usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombres` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password_user` text NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  `fyh_creacion` datetime NOT NULL,
+  `fyh_actualizacion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tb_usuarios`
+--
+
+INSERT INTO `tb_usuarios` (`id_usuario`, `nombres`, `email`, `password_user`, `token`, `id_rol`, `fyh_creacion`, `fyh_actualizacion`) VALUES
+(1, 'Leonel Ventura', 'leonelVen@gmail.com', '$2y$10$uUF.9RAOI.GHNuHO6Vnv4.D.x1V6wknq9aKsCmYAuN9rbMVSpCave', '', 1, '2025-06-01 22:37:49', '2025-06-01 22:37:49');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tb_almacen`
+--
+ALTER TABLE `tb_almacen`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
+-- Indices de la tabla `tb_categorias`
+--
+ALTER TABLE `tb_categorias`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `tb_roles`
+--
+ALTER TABLE `tb_roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `id_rol` (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tb_almacen`
+--
+ALTER TABLE `tb_almacen`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tb_categorias`
+--
+ALTER TABLE `tb_categorias`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `tb_roles`
+--
+ALTER TABLE `tb_roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tb_almacen`
+--
+ALTER TABLE `tb_almacen`
+  ADD CONSTRAINT `tb_almacen_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `tb_categorias` (`id_categoria`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_almacen_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  ADD CONSTRAINT `tb_usuarios_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `tb_roles` (`id_rol`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
