d76db1a5245de77aed28bf7eb4f909c3ff866395
comander7217 <christian_vd21@hotmail.com>
2025-06-05 00:21:10 -0500

    se modifico el layout

--- layout/sesion.php
+++ layout/sesion.php
@@ -8,6 +8,7 @@ if(isset($_SESSION['sesion email'])){
   $query->execute();
   $usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
   foreach ($usuarios as $usuario){
+    $id_usuario_sesion = $usuario['id_usuario'];
     $nombres_sesion = $usuario['nombres'];
   }
   
