05ba78d65952b013f4d8d7740a68f118c41397d6
Nieves <elizzaync@gmail.com>
2025-06-07 06:02:12 -0500

    se puede borrar productos desde el formulario

--- almacen/delete.php
+++ almacen/delete.php
@@ -4,7 +4,6 @@ include ('../layout/sesion.php');
 
 include ('../layout/parte1.php');
 
-include ('../app/controllers/categorias/listado_de_categoria.php');
 include ('../app/controllers/almacen/cargar_producto.php');
 
 foreach ($productos_datos as $productos_dato) {
@@ -22,6 +21,7 @@ foreach ($productos_datos as $productos_dato) {
     $imagen = $productos_dato['imagen'];
 }
 
+
 ?>
 
 <!-- Content Wrapper. Contains page content -->
@@ -31,7 +31,7 @@ foreach ($productos_datos as $productos_dato) {
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1 class="m-0">Actualizar producto</h1>
+                    <h1 class="m-0">Datos del producto: <?php echo $nombre;?> a ser eliminado</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -45,9 +45,9 @@ foreach ($productos_datos as $productos_dato) {
 
             <div class="row">
                 <div class="col-md-12">
-                    <div class="card card-success">
+                    <div class="card card-danger">
                         <div class="card-header">
-                            <h3 class="card-title">Llene los datos con cuidado</h3>
+                            <h3 class="card-title">¿Esta seguro de eliminar el producto?</h3>
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
                                 </button>
@@ -58,10 +58,8 @@ foreach ($productos_datos as $productos_dato) {
                         <div class="card-body" style="display: block;">
                             <div class="row">
                                 <div class="col-md-12">
-
-                                    <form action="../app/controllers/almacen/update.php" method="post" enctype="multipart/form-data">
-                                        <input type="text" value="<?php echo $id_producto_get; ?>" name="id_producto" hidden>
-
+                                    <form action="../app/controllers/almacen/delete.php" method="post">
+                                        <input type="text" name="id_producto" value="<?php echo $id_producto_get;?>" hidden>
                                         <div class="row">
                                             <div class="col-md-9">
                                                 <div class="row">
@@ -70,33 +68,20 @@ foreach ($productos_datos as $productos_dato) {
                                                             <label for="">Código:</label>
                                                             <input type="text" class="form-control"
                                                                    value="<?php echo $codigo; ?>" disabled>
-                                                            <input type="text"  name="codigo" value="<?php echo $codigo; ?>" hidden>
                                                         </div>
                                                     </div>
                                                     <div class="col-md-4">
                                                         <div class="form-group">
                                                             <label for="">Categoría:</label>
                                                             <div style="display: flex">
-                                                                <select name="id_categoria" id="" class="form-control" required>
-                                                                    <?php
-                                                                    foreach ($categorias_datos as $categorias_dato){
-                                                                        $nombre_categoria_tabla = $categorias_dato['nombre_categoria'];
-                                                                        $id_categoria = $categorias_dato['id_categoria']?>
-                                                                        <option value="<?php echo $id_categoria; ?>"<?php if($nombre_categoria_tabla == $nombre_categoria){ ?> selected="selected" <?php } ?> >
-                                                                            <?php echo $nombre_categoria_tabla;?>
-                                                                        </option>
-                                                                        <?php
-                                                                    }
-                                                                    ?>
-                                                                </select>
-
+                                                                <input type="text" class="form-control" value="<?php echo $nombre_categoria; ?>" disabled>
                                                             </div>
                                                         </div>
                                                     </div>
                                                     <div class="col-md-4">
                                                         <div class="form-group">
                                                             <label for="">Nombre del producto:</label>
-                                                            <input type="text" name="nombre" value="<?php echo $nombre;?>" class="form-control" required>
+                                                            <input type="text" name="nombre" value="<?php echo $nombre;?>" class="form-control" disabled>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -106,13 +91,12 @@ foreach ($productos_datos as $productos_dato) {
                                                         <div class="form-group">
                                                             <label for="">Usuario</label>
                                                             <input type="text" class="form-control" value="<?php echo $email; ?>" disabled>
-                                                            <input type="text" name="id_usuario" value="<?php echo $id_usuario; ?>" hidden>
                                                         </div>
                                                     </div>
                                                     <div class="col-md-8">
                                                         <div class="form-group">
                                                             <label for="">Descripción del producto:</label>
-                                                            <textarea name="descripción" id="" cols="30" rows="2" class="form-control"><?php echo $descripcion;?></textarea>
+                                                            <textarea name="descripcion" id="" cols="30" rows="2" class="form-control" disabled><?php echo $descripcion;?></textarea>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -122,37 +106,37 @@ foreach ($productos_datos as $productos_dato) {
                                                     <div class="col-md-2">
                                                         <div class="form-group">
                                                             <label for="">Stock:</label>
-                                                            <input type="number" name="stock" value="<?php echo $stock;?>" class="form-control" required>
+                                                            <input type="number" name="stock" value="<?php echo $stock;?>" class="form-control" disabled>
                                                         </div>
                                                     </div>
                                                     <div class="col-md-2">
                                                         <div class="form-group">
                                                             <label for="">Stock mínimo:</label>
-                                                            <input type="number" name="stock_minimo" value="<?= $stock_minimo; ?>" class="form-control">
+                                                            <input type="number" name="stock_minimo" value="<?php echo $stock_minimo;?>" class="form-control" disabled>
                                                         </div>
                                                     </div>
                                                     <div class="col-md-2">
                                                         <div class="form-group">
                                                             <label for="">Stock máximo:</label>
-                                                            <input type="number" name="stock_maximo" value="<?= $stock_maximo; ?>" class="form-control">
+                                                            <input type="number" name="stock_maximo" value="<?php echo $stock_maximo;?>" class="form-control" disabled>
                                                         </div>
                                                     </div>
                                                     <div class="col-md-2">
                                                         <div class="form-group">
                                                             <label for="">Precio compra:</label>
-                                                            <input type="number" name="precio_compra" value="<?= $precio_compra; ?>" class="form-control" required>
+                                                            <input type="number" name="precio_compra" value="<?php echo $precio_compra;?>" class="form-control" disabled>
                                                         </div>
                                                     </div>
                                                     <div class="col-md-2">
                                                         <div class="form-group">
                                                             <label for="">Precio venta:</label>
-                                                            <input type="number" name="precio_venta" value="<?= $precio_venta; ?>" class="form-control" required>
+                                                            <input type="number" name="precio_venta" value="<?php echo $precio_venta;?>" class="form-control" disabled>
                                                         </div>
                                                     </div>
                                                     <div class="col-md-2">
                                                         <div class="form-group">
                                                             <label for="">Fecha de ingreso:</label>
-                                                            <input type="date" name="fecha_ingreso" value="<?= $fecha_ingreso; ?>" class="form-control" required>
+                                                            <input type="date" name="fecha_ingreso" value="<?php echo $fecha_ingreso;?>" class="form-control" disabled>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -162,33 +146,9 @@ foreach ($productos_datos as $productos_dato) {
                                             <div class="col-md-3">
                                                 <div class="form-group">
                                                     <label for="">Imagen del producto</label>
-                                                    <input type="file" name="image" class="form-control" id="file">
-                                                    <input type="text" name="image_text" value="<?php echo $imagen; ?>" hidden>
-                                                    <br>
-                                                    <output id="list" style="">
-                                                        <img src="<?php echo $URL."/almacen/img_productos/".$imagen; ?>" width="100%" alt="">
-                                                    </output>
-                                                    <script>
-                                                        function archivo(evt) {
-                                                            var files = evt.target.files; // FileList object
-                                                            // Obtenemos la imagen del campo "file".
-                                                            for (var i = 0, f; f = files[i]; i++) {
-                                                                //Solo admitimos imágenes.
-                                                                if (!f.type.match('image.*')) {
-                                                                    continue;
-                                                                }
-                                                                var reader = new FileReader();
-                                                                reader.onload = (function (theFile) {
-                                                                    return function (e) {
-                                                                        // Insertamos la imagen
-                                                                        document.getElementById("list").innerHTML = ['<img class="thumb thumbnail" src="',e.target.result, '" width="100%" title="', escape(theFile.name), '"/>'].join('');
-                                                                    };
-                                                                })(f);
-                                                                reader.readAsDataURL(f);
-                                                            }
-                                                        }
-                                                        document.getElementById('file').addEventListener('change', archivo, false);
-                                                    </script>
+                                                    <center>
+                                                        <img src="<?php echo $URL."/almacen/img_productos/".$imagen;?>" width="100%" alt="">
+                                                    </center>
                                                 </div>
                                             </div>
                                         </div>
@@ -200,7 +160,7 @@ foreach ($productos_datos as $productos_dato) {
                                         <hr>
                                         <div class="form-group">
                                             <a href="index.php" class="btn btn-secondary">Cancelar</a>
-                                            <button type="submit" class="btn btn-success">Actualizar producto</button>
+                                            <button class="btn btn-danger"><i class="fa fa-trash"></i> Borrar producto</button>
                                         </div>
                                     </form>
                                 </div>
